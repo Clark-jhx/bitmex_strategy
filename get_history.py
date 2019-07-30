2a232864da4bcba06b0ec681bddaac550154ac80
Clark_jiang <996719704@qq.com>
2019-07-30 18:16:02 +0800

    Update get_history.py

--- get_history.py
+++ get_history.py
@@ -17,40 +17,73 @@ def test():
     print(lists)
     write_to_file(lists)
 
+# 一个月的数据
+def one_month_history():
+    start_time = datetime.datetime(2019, 1, 1, 11, 59)
+    end_time = datetime.datetime(2019, 1, 31, 23, 59)
+    trades = []
+    times = get_times()
+    for time in times:
+        if (start_time <= time) and (time < end_time):
+            print(time)
+            trade = get_trade_history(time=time, count=720)
+            reversed_list = list(reversed(trade))
+            trades += reversed_list
+    file_name = '{}-{}.json'.format(start_time.year, start_time.month)
+    write_to_file(trades, file_name=file_name)
+    pass
 
-def write_to_file(lists):
+
+# 所有历史数据写入文件，以月为单位
+def history_to_file():
+    times = get_times()
+    trades = []
+    year = times[0].year
+    month = times[0].month
+    for time in times:
+        if (year != time.year) or (month != time.month):
+            file_name = '{}-{}.json'.format(year, month)
+            write_to_file(trades, file_name=file_name)
+            print('生成文件 {}'.format(file_name))
+            trades.clear()
+        print(time)
+        trade = get_trade_history(time=time, count=720)
+        reversed_list = list(reversed(trade))
+        trades += reversed_list
+        year = time.year
+        month = time.month
+
+# 将列表写入文件
+def write_to_file(lists, file_name='data.json'):
     lists_str = '{history:' + lists.__str__() + '}'
     lists_str = lists_str.replace('\n', '')  # 去掉空格
 
-    with open('data.json', 'w') as f:
+    with open(file_name, 'w') as f:
         json.dump(lists_str, f)
 
 
-def history_to_file():
-    times = get_times_2()
-    trades = []
-    for time in times:
-        print(time)
-        trade = get_trade_history(time=time, count=720)
-        trades += trade
-    trades.reverse()
-    write_to_file(trades)
-
-
-## 获取时间列表
-def get_times(start_year=2017, start_month=1, start_day=1, start_hour=0, start_minuter=0, delta_hour=12):
+## 获取服务器时间列表
+def get_times(start_year=2017, start_month=1, start_day=1, start_hour=11, start_minuter=59, delta_hour=12):
     start_time = datetime.datetime(start_year, start_month, start_day, start_hour, start_minuter, 0, 0)
-    times = [start_time.strftime('%Y-%m-%d %H:%M')]
+    times = []
     var = 0
-    end_time = datetime.datetime.now()
+    end_time = datetime.datetime.now() - datetime.timedelta(days=1)
     while True:
-        var += 1
         next_time = start_time + datetime.timedelta(hours=delta_hour * var)
         if next_time > end_time:
             return times
             break
-        times.append(next_time.strftime('%Y-%m-%d %H:%M'))
+        times.append(next_time)
+        var += 1
 
+## 处理服务器的时间差 8天8小时
+def to_server_time(time):
+    server_time = time - datetime.timedelta(days=8) - datetime.timedelta(hours=8)
+    return server_time
+
+def to_normal_time(time):
+    normal_time = time + datetime.timedelta(days=8) + datetime.timedelta(hours=8)
+    return normal_time
 
 ## 获取时间列表
 def get_times_2(start_year=2019, start_month=7, start_day=20, start_hour=0, start_minuter=0, delta_hour=12):
@@ -68,7 +101,7 @@ def get_times_2(start_year=2019, start_month=7, start_day=20, start_hour=0, star
 
 
 ## 参考https://www.bitmex.com/api/explorer/#!/Trade/Trade_getBucketed
-def get_trade_history(time='2019-7-20 00:00 UTC+8:00', count=4.0, symbol='XBTUSD', bin_size='1m'):
+def get_trade_history(time=None, count=4.0, symbol='XBTUSD', bin_size='1m'):
     api_client = ApiClient(header_name='Accept', header_value='application/json')
     api_client.configuration.host = 'https://www.bitmex.com/api/v1'
     api_instance = swagger_client.TradeApi(api_client=api_client)
@@ -77,11 +110,11 @@ def get_trade_history(time='2019-7-20 00:00 UTC+8:00', count=4.0, symbol='XBTUSD
     symbol = symbol  # str | Instrument symbol. Send a bare series (e.g. XBU) to get data for the nearest expiring contract in that series.  You can also send a timeframe, e.g. `XBU:monthly`. Timeframes are `daily`, `weekly`, `monthly`, `quarterly`, and `biquarterly`. (optional)
     filter = ''  # str | Generic table filter. Send JSON key/value pairs, such as `{\"key\": \"value\"}`. You can key on individual fields, and do more advanced querying on timestamps. See the [Timestamp Docs](https://www.bitmex.com/app/restAPI#Timestamp-Filters) for more details. (optional)
     columns = ''  # str | Array of column names to fetch. If omitted, will return all columns.  Note that this method will always return item keys, even when not specified, so you may receive more columns that you expect. (optional)
-    count = 3.0  # float | Number of results to fetch. (optional) (default to 100)
+    count = count  # float | Number of results to fetch. (optional) (default to 100)
     start = 0.0  # float | Starting point for results. (optional) (default to 0)
     reverse = False  # bool | If true, will sort results newest first. (optional) (default to false)
-    temp = datetime.datetime(2017, 1, 1, 0, 0, tzinfo=tzlocal())
-    start_time = time  # datetime | Starting date filter for results. (optional)
+    server_time = to_server_time(time).strftime('%Y-%m-%d %H:%M')
+    start_time = server_time  # datetime | Starting date filter for results. (optional)
     end_time = ''  # datetime | Ending date filter for results. (optional)
 
     try:
@@ -89,7 +122,6 @@ def get_trade_history(time='2019-7-20 00:00 UTC+8:00', count=4.0, symbol='XBTUSD
         api_response = api_instance.trade_get_bucketed(bin_size=bin_size, partial=partial, symbol=symbol,
                                                        columns=columns, count=count, start=start, reverse=reverse,
                                                        start_time=start_time)
-        #print(api_response)
     except ApiException as e:
         print("Exception when calling TradeApi->trade_get_bucketed: %s\n" % e)
     return api_response
@@ -97,6 +129,5 @@ def get_trade_history(time='2019-7-20 00:00 UTC+8:00', count=4.0, symbol='XBTUSD
 
 if __name__ == '__main__':
     history_to_file()
+    #one_month_history()
     #test()
-
-
